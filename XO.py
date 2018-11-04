ad4b2596fc225074ef05d421cc486e09f75fce2c
galc <gal.c@sygnia.co>
2018-11-04 19:51:11 +0200

    fixed all PR comments

--- XO.py
+++ XO.py
@@ -1,158 +1,157 @@
-from random import shuffle
+from random import randint
+from collections import namedtuple
 
-# size of board
+# Size of board.
 N = 3
 
-# Utility functions
+
+class Model:
+    def __init__(self):
+        self.board = [[None for x in range(N)] for y in range(N)]
+
+    def get_board_status(self):
+        return self.board
+
+    def update_board(self, line, col, players, curr_player):
+        if self.board[line][col] is not None:
+            return False
+        else:
+            self.board[line][col] = players[curr_player].symbol
+            return True
+
+    def line_won(self, players, curr_player):
+        for line in self.board:
+            if all(x == players[curr_player].symbol for x in line):
+                return True
+        return False
+
+    def col_won(self, players, curr_player):
+        for col in range(N):
+            if all(line[col] == players[curr_player].symbol for line in self.board):
+                return True
+        return False
+
+    def diagonal_won(self, players, curr_player):
+        # Main diagonal.
+        if all(self.board[index][index] == players[curr_player].symbol for index in range(N)):
+            return True
+        # Opposite diagonal.
+        if all(self.board[index][N-1-index] == players[curr_player].symbol for index in range(N)):
+            return True
+        return False
 
 
-def print_board(board):
+class View:
+    def __init__(self):
+        pass
 
-    print()
-    print('Board:')
-    for line_ in board:
-        for item in line_:
-            print(item, end='')
+    def print_board(self, board):
         print()
-    print()
+        print('Board:')
+        for line in board:
+            for item in line:
+                if item is None:
+                    print('-', end='')
+                else:
+                    print(item, end='')
+            print()
+        print()
+
+    def declare_winner(self, players, curr_player):
+        print()
+        print('***')
+        print(f'The winner is {players[curr_player].name}!!!')
+        print('***')
+
+    def declare_tie(self):
+        print()
+        print('***')
+        print('The result is a tie!!!')
+        print('***')
 
 
-def check_input_validity(input_type, curr_player):
-    not_valid = True
-    while not_valid:
-        print('{}, please insert the {} number'.format(curr_player, input_type))
-        try:
-            input_value = int(input())
-        except:
-            print('{}, please insert a valid {} number'.format(curr_player, input_type))
-            continue
-        if input_value < 0 or input_value >= N:
-            print('{}, please insert a valid {} number'.format(curr_player, input_type))
-            continue
-        not_valid = False
-    return input_value
+class Controller:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+        self.players, self.curr_player = self.initiate_players()
+        self.number_of_moves_left = N * N  # Equals to number of squares on board.
+
+    def initiate_players(self):
+        Player = namedtuple('Player', 'name symbol')
+        x_player_name = input('Enter X player name\n')
+        o_player_name = input('Enter O player name\n')
+        while x_player_name == o_player_name:
+            o_player_name = input('Both names are the same. Choose a different name for O player\n')
+        players = [Player(name=x_player_name, symbol='x'), Player(name=o_player_name, symbol='o')]
+        curr_player = randint(0, 1)
+        return players, curr_player
+
+    def show_board(self):
+        board = self.model.get_board_status()
+        self.view.print_board(board)
+
+    def get_input(self):
+        line = self.is_input_valid('line')
+        col = self.is_input_valid('column')
+        updated = self.model.update_board(line, col, self.players, self.curr_player)
+        if not updated:
+            print(f'{self.players[self.curr_player].name}, you entered a taken spot on the board, try again')
+            return False
+        return True
+
+    def is_input_valid(self, input_type):
+        valid_input = False
+        while not valid_input:
+            print(f'{self.players[self.curr_player].name}, please insert the {input_type} number')
+            try:
+                input_value = int(input())
+            except ValueError:
+                print(f'{self.players[self.curr_player].name}, input not valid')
+                continue
+            if input_value < 0 or input_value >= N:
+                print(f'{self.players[self.curr_player].name}, insert a {input_type} num between 0 to {N-1}, including')
+            else:
+                valid_input = True
+        return input_value
+
+    def switch_player(self):
+        self.curr_player = self.curr_player ^ 1
+
+    def is_win(self):
+        if self.model.line_won(self.players, self.curr_player) \
+                or self.model.col_won(self.players, self.curr_player) \
+                or self.model.diagonal_won(self.players, self.curr_player):
+            self.view.declare_winner(self.players, self.curr_player)
+            self.show_board()
+            return True
+        return False
+
+    def is_tie(self):
+        self.number_of_moves_left -= 1
+        if self.number_of_moves_left == 0:
+            self.view.declare_tie()
+            self.show_board()
+            return True
+        return False
+
+    def main(self):
+        game_won = False
+        game_tie = False
+        while not (game_won or game_tie):
+            # Print current board status.
+            controller.show_board()
+            # Get input for next move.
+            got_input_successfully = controller.get_input()
+            while not got_input_successfully:
+                got_input_successfully = controller.get_input()
+            # Check if player won.
+            game_won = controller.is_win()
+            # Check if tie.
+            game_tie = controller.is_tie()
+            # Switch player.
+            controller.switch_player()
 
 
-def check_line(board, line, curr_player, curr_player_symbol, is_win):
-    for sym in board[line]:
-        if sym != curr_player_symbol:
-            is_win = False
-            break
-    if is_win:
-        return declare_win(curr_player)
-
-
-def check_col(board, col, curr_player, curr_player_symbol, is_win):
-    for line in board:
-        if line[col] != curr_player_symbol:
-            is_win = False
-            break
-    if is_win:
-        return declare_win(curr_player)
-
-
-def check_main_diagonal(board, line, col, curr_player, curr_player_symbol, is_win):
-    if line == col:
-        for i in range(N):
-            if board[i][i] != curr_player_symbol:
-                is_win = False
-                break
-    else:
-        is_win = False
-
-    if is_win:
-        return declare_win(curr_player)
-
-
-def check_opposite_diagonal(board, line, col, curr_player, curr_player_symbol, is_win):
-    if line + col == N - 1:
-        j = 2
-        for i in range(N):
-            if j < 0:
-                is_win = False
-                break
-            if board[i][j] != curr_player_symbol:
-                is_win = False
-                break
-            j-=1
-    else:
-        is_win = False
-
-    if is_win:
-        return declare_win(curr_player)
-
-
-def declare_win(curr_player):
-    print('The winner is {}!!!'.format(curr_player))
-    print_board(board)
-    return(True)
-
-
-def switch_users(curr_player_, x_player, o_player):
-    if curr_player_ == x_player:
-        curr_player_ = o_player
-        curr_player_sym = 'o'
-    else:
-        curr_player_ = x_player
-        curr_player_sym = 'x'
-    return curr_player_, curr_player_sym
-
-
-# initiate board
-board = [['-' for x in range(N)] for y in range(N)]
-number_of_moves_left = N * N  # number of moves equals to the number of squares on the board
-
-# initiate players
-player_1 = input('Hello! Please enter the first player name\n')
-player_2 = input('Please enter the second player name\n')
-
-while player_1 == player_2:
-    player_2 = input('Both names are the same. Please choose a different name for second player\n')
-
-# choose 1st player randomly
-players = [player_1, player_2]
-shuffle(players)
-x_player = players[0]
-o_player = players[1]
-curr_player = x_player
-curr_player_symbol = 'x'
-
-print('The X player is {}. You start!'.format(x_player))
-print('The O player is {}'.format(o_player))
-
-
-# main loop of the game
-
-while True:
-    # print a fancy board
-    print_board(board)
-
-    # get input for next move
-    line = check_input_validity('line', curr_player)
-    col = check_input_validity('column', curr_player)
-
-    if board[line][col] != '-':
-        print('{}, you entered a taken spot on the board'.format(curr_player))
-        continue
-    else:
-        board[line][col] = curr_player_symbol
-
-    # check if win
-    if check_line(board, line, curr_player, curr_player_symbol, True):
-        break
-    if check_col(board, col, curr_player, curr_player_symbol, True):
-        break
-    if check_main_diagonal(board, line, col, curr_player, curr_player_symbol, True):
-        break
-    if check_opposite_diagonal(board, line, col, curr_player, curr_player_symbol, True):
-        break
-
-    # check if tie
-    number_of_moves_left-=1
-    if number_of_moves_left == 0:
-        print('Result is a tie!')
-        print_board(board)
-        break
-
-    # switch users
-    curr_player, curr_player_symbol = switch_users(curr_player, x_player, o_player)
+controller = Controller(Model(), View())
+controller.main()
