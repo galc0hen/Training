6cba429ebe632547a0dc4645fee930989811b99c
galc <gal.c@sygnia.co>
2018-11-17 20:06:48 +0200

    Fixed all PR comments.

--- mult.py
+++ mult.py
@@ -1,35 +1,31 @@
-def least_common_multiple(big_num, small_num):
-    count = 1
+def smallest_dividend(num_1, num_2):
+    """
+    Smallest dividend is defined here
+    as the smallest number that can be divided
+    by both num_1 and num_2.
+    E.g.,
+    3, 5 -> 15
+    2, 4 -> 4
+    8, 12 -> 24
+    """
+    big_num = max(num_1, num_2)
+    small_num = min(num_1, num_2)
+    counter = 1
     while True:
-        if (big_num * count) % small_num == 0:
-            return big_num * count
-        else:
-            count += 1
+        if (big_num * counter) % small_num == 0:
+            return big_num * counter
+        counter += 1
+
+
+def print_result(num_1, num_2, result):
+    print(f'The smallest dividend of {num_1} and {num_2} is {result}')
 
 
 def main():
-    # read numbers from user
-    while True:
-        try:
-            a = int(input('enter first number\n'))
-        except ValueError:
-            print('enter valid input')
-            continue
-        else:
-            break
-
-    while True:
-        try:
-            b = int(input('enter second number\n'))
-        except ValueError:
-            print('enter valid input')
-            continue
-        else:
-            break
-    big_num = max(a, b)
-    small_num = min(a,b)
-    least_common_multiple = least_common_multiple(big_num, small_num)
-    print(f'Least common multiplier of {big_num} and {small_num} is {least_common_multiple}')
+    print_result(3, 5, smallest_dividend(3, 5))  # Result is 15.
+    print_result(2, 4, smallest_dividend(2, 4))  # Result is 4.
+    print_result(8, 12, smallest_dividend(8, 12))  # Result is 24.
 
 
-main()
+if __name__ == '__main__':
+    main()
