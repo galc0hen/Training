4f40581c28cb078e7cad962a929006842f166ded
galc0hen <gal.c@sygnia.co>
2019-01-14 11:09:57 +0200

    Submitting new exercise - stdout_9

--- stdout_9.py
+++ stdout_9.py
@@ -9,20 +9,19 @@ class MyBuffer:
     def __enter__(self):
         self.open_buffer = io.StringIO()
         sys.stdout = self.open_buffer
-        # self.open_file = open(self.filename, self.mode)
-        # return self.open_buffer
+        return self.open_buffer
 
     def __exit__(self, *args):
         self.open_buffer.close()
-        return self.original_stdout
+        sys.stdout = self.original_stdout
 
 
 def main():
-    sys.stdout.write('foo')
-    with MyBuffer() as outfile:
-        sys.stdout.write('foo')
-        # outfile.write('foo')
-        print(outfile.read())
+    with MyBuffer() as buffer_output:
+        print('test output')  # Does not print to screen - the output goes to buffer.
+        output_inside_context_manager = buffer_output.getvalue()
+    # Print 'test output' outside the context manager.
+    print(f'Printing the buffer output outside the context manager: {output_inside_context_manager}')
 
 
 if __name__ == '__main__':
