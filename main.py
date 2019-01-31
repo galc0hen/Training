d47c7f6a2769e26897fbf205521e9ada66f83a9c
galc0hen <gal.c@sygnia.co>
2019-01-31 14:30:03 +0200

    Submitting 1st part of Game of DBs - 12th exercise

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ BUFFER_SIZE = 100
 def main():
     data_generator = s1wide()
     entry_list = []
+    # I chose to implement some of the indexing logic here because in module implement_me.py,
+    # the func "index" is said to get as a parameter a list of 'Entry' instances to index.
     for entry in data_generator:
         entry_list.append(entry)
         if len(entry_list) >= BUFFER_SIZE:
@@ -17,7 +19,8 @@ def main():
 
     # Expected output: 16384 (128 unique IPs * 128 entries per IP).
     print(f'Number of DB entries: {GLOBAL_SESSION.query(Ip).count()}')
-    # Expected output: list of 2 dicts, each containing a timestamp and a protocol of the given IP.
+    # Expected output: list of 2 dicts, each containing a timestamp and a protocol for the given IP.
+    # Constant IP 113.79.183.223 can be used for testing because Random was given a seed of 0 (in datasets.py).
     print(get_device_histogram('113.79.183.223', 2))
     # Expected output: list of 128 tuples, each containing an IP and a timestamp.
     print(get_devices_status())
